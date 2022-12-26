7f70cb6784caf8c8bf962128c9b3279f42a6b200
Jonas Almeida <jalmeida@mathbiol.org>
2022-12-26 18:05:16 -0500

    match filter with a floor slice

---
+++ export.js
@@ -0,0 +1,12 @@
+console.log('importing https://usm.github.io/diff/export.js');
+
+const hello = 'hello USMdiff';
+
+let usm = function(seq,abc){
+    return new async function(){}
+}
+
+export {
+    hello,
+    usm
+}
