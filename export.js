c096bf2543597a1723812ce19d67c41627bb376b
Jonas Almeida <jalmeida@mathbiol.org>
2022-12-27 16:09:20 -0500

    USM class
    remember a class in JS is still about prototypal inheritance: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Classes

--- export.js
+++ export.js
@@ -2,11 +2,14 @@ console.log('importing https://usm.github.io/diff/export.js');
 
 const hello = 'hello USMdiff';
 
-let usm = function(seq,abc){
-    return new async function(){}
+class USM {
+    constructor (seq='accgtgtgacgatggca',abc='acgt'){
+        this.seq=seq
+        this.abc=abc
+    } 
 }
 
 export {
     hello,
-    usm
+    USM
 }
