4dbdb5ad5e1cf6b0a107a53b7c6c55c47ec5a330
Jonas Almeida <jalmeida@mathbiol.org>
2023-02-23 14:54:22 -0500

    resume dev

--- USMdiff.js
+++ USMdiff.js
@@ -1,6 +1,7 @@
 (async ()=>{
-    USMdiff = await import('./export.js')
+    //diff = await import('./export.js')
+    diff = await import('https://usm.github.io/diff/export.js')
     if(typeof(define)!='undefined'){
-        define(USMdiff)
+        define(diff)
     }
 })()
