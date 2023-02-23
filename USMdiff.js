5bdb6017868a73d803accd500065ee730f7e2544
Jonas Almeida <jalmeida@mathbiol.org>
2023-02-23 16:22:05 -0500

    TP53 ini

--- USMdiff.js
+++ USMdiff.js
@@ -1,5 +1,5 @@
 (async ()=>{
-    //diff = await import('./export.js')
+    //diff = await import('http://localhost:8000/diff/export.js')
     diff = await import('https://usm.github.io/diff/export.js')
     if(typeof(define)!='undefined'){
         define(diff)
--- export.js
+++ export.js
@@ -22,6 +22,43 @@ async function getFasta(url='https://www.ebi.ac.uk/ena/browser/api/fasta/OD91476
     return fa
 }
 
+const proxy = 'https://proxy.jalmeida.workers.dev/?'
+
+// fetch TP53 data - START - //
+
+async function getProxyCSV(url='https://storage.googleapis.com/tp53-static-files/data/GermlineDownload_r20.csv'){
+    url = proxy+url
+    let csv = await (await fetch(url)).text()
+    return csv2json(csv)
+}
+
+function csv2json(csv){
+    csv=csv.replace('"','').replace(/\\n"$/,'') // deblank specific to TP53 csv
+    let rows = csv.split(/\\n/g)
+    rows = rows.map(r=>{
+        return r.split(',')
+    })
+    let cols=rows[0]
+    rows=rows.slice(1)
+    let json={}
+    cols.forEach((c,i)=>{
+        json[c]=[]
+        rows.forEach((r,j)=>{
+            json[c][j]=r[i]
+        })
+    })
+    return json
+}
+
+async function fetchTP53data(url='https://storage.googleapis.com/tp53-static-files/data/GermlineDownload_r20.csv'){
+    console.log(`fetching TP53 data from ${url}`)
+    url = proxy+url
+    let csv = await (await fetch(url)).text()
+    return csv2json(csv)
+}
+
+// fetch TP53 data - START - //
+
 function unique(str){
     let y={}
     str.split('').forEach(s=>{
@@ -34,5 +71,6 @@ export {
     hello,
     USM,
     unique,
-    getFasta
+    getFasta,
+    fetchTP53data
 }
